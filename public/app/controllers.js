b942f64c4ccd7fa371054a7f2c68a8dc82421a48
rfprod <zoidenmacht@zoho.com>
2017-10-28 12:56:56 -0700

    (update) blog controller - improved parsing of track description from soundcloud

--- public/app/controllers.js
+++ public/app/controllers.js
@@ -407,7 +407,9 @@ dnbhubControllers.controller('blogCtrl', ['$scope', '$sce', '$route', '$location
 			*	\n to <br/>
 			*	links to anchors
 			*/
-			var processed = unprocessed.replace(/\n/g, '<br/>').replace(/(http(s)?:\/\/(www\.)?[a-zA-Z0-9][-a-zA-Z0-9@:%._+~#=]{0,255}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*))/g, '<a href="$1" target=_blank><i class="fa fa-external-link"></i> <span class="md-caption">$1</span></a>');
+			var processed = unprocessed.replace(/\n/g, '<br/>')
+				.replace(/(http(s)?:\/\/(www\.)?[a-zA-Z0-9][-a-zA-Z0-9@:%._+~#=]{0,255}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*))/g, '<a href="$1" target=_blank><i class="fa fa-external-link"></i> <span class="md-caption">$1</span></a>')
+				.replace(/(@)([^\s<)\]]+)/g, '<a href="https://soundcloud.com/$2" target=_blank><i class="fa fa-external-link"></i> <span class="md-caption">$1$2</span></a>');
 			// console.log('processed', processed);
 			return processed;
 		};
