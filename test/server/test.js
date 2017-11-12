5f9e80584bbdb508d81af3f52d6e8820cf94e039
rfprod <zoidenmacht@zoho.com>
2017-11-12 06:29:33 -0800

    (update) server test syntax

--- test/server/test.js
+++ test/server/test.js
@@ -5,30 +5,25 @@ const request = require('request');
 const cheerio = require('cheerio'),
 	str = require('string');
 
-require('dotenv').load();
 const baseUrl = 'http://localhost:3000';
 
-describe('/ endpoint', function() {
-	it('should load a an angular initialization page', function (done) {
-		request(baseUrl+'/', function (error,response,body) {
+describe('/ endpoint', () => {
+	it('should load a an angular initialization page', async () => {
+		request(baseUrl + '/', (error,response,body) => {
 
 			expect(error).to.be.not.ok;
 			expect(response).to.be.not.a('undefined');
 			expect(response.statusCode).to.be.equal(200);
 
-			done();
 		});
 	});
-	it('should deliver index page with elements with logo and \'view-frame\' classes', function (done) {
-		request(baseUrl+'/', function (error,response,body) {
+	it('should deliver index page with elements with navbar and \'view-frame\' classes', async () => {
+		request(baseUrl + '/', (error,response,body) => {
 
 			const $ = cheerio.load(body);
-			setTimeout(function() {
-				assert.equal(1, $('img.logo').length);
-				assert.equal(1, $('div.view-frame').length);
-			}, 5000);
+			assert.equal(1, $('div[data-ng-include]').length);
+			assert.equal(1, $('div.view-frame').length);
 
-			done();
 		});
 	});
 });
