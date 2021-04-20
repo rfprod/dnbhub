8e329f86898bbfbe0e9df1da790b81d41748925c
Vadim <rfprod@users.noreply.github.com>
2021-04-20 23:21:17 +0300

    fix(client): fix client app (#62)

--- src/app/guards/authenticated/authenticated.guard.ts
+++ src/app/guards/authenticated/authenticated.guard.ts
@@ -1,28 +1,23 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
 import { Store } from '@ngxs/store';
-import { Observable } from 'rxjs';
-import { map, tap } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 
 import { DnbhubFirebaseState } from '../../state/firebase/firebase.store';
 
-/**
- * Authenticated guard.
- * Passes authenticated users only.
- */
 @Injectable({
   providedIn: 'root',
 })
 export class DnbhubAuthenticatedGuard implements CanActivate {
   constructor(private readonly router: Router, private readonly store: Store) {}
 
-  public canActivate(): Observable<boolean> {
+  public canActivate() {
     return this.store.selectOnce(DnbhubFirebaseState.getState).pipe(
-      map(state => state.userInfo !== null),
-      tap(activate => {
-        if (!activate) {
-          void this.router.navigate(['index']);
+      map(state => {
+        if (state.userInfo !== null) {
+          return true;
         }
+        return this.router.createUrlTree(['/index']);
       }),
     );
   }
--- src/app/state/firebase/firebase.actions.ts
+++ src/app/state/firebase/firebase.actions.ts
@@ -3,6 +3,7 @@ import { getActionCreator } from 'src/app/utils/ngxs.util';
 import {
   FIREBASE_STATE_TOKEN,
   TDnbhubFirebasePayload,
+  TEmailSignInPayload,
   TResetPasswordPayload,
 } from './firebase.interface';
 
@@ -12,7 +13,13 @@ const setState = createAction<TDnbhubFirebasePayload>('set state');
 
 const sendPasswordResetEmail = createAction<TResetPasswordPayload>('send password reset email');
 
+const emailSignIn = createAction<TEmailSignInPayload>('email sign in');
+
+const signOut = createAction('sign out');
+
 export const firebaseActions = {
   setState,
   sendPasswordResetEmail,
+  emailSignIn,
+  signOut,
 };
